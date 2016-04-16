a5ca6bdfd934e7dbf359ced86e10dd55c66aa747
Leonard Schmischke <leonard.schmischke@mni.thm.de>
2016-04-16 21:37:37 +0200

    Begonnen, die User-Datenbank zu implementieren

--- server/user_databases.adb
+++ server/user_databases.adb
@@ -1,18 +1,147 @@
 package body User_Databases is
 
-   function getUser(this : in out User_Database; userToGet : in User) return User is
+   function registerUser(this : in out User_Database; username : in String; password : in String) return Boolean is
+      user : UserPtr;
+      bool : Boolean;
+      contacts : ContactList.List;
+
    begin
-      return userToGet;
+      setUsername(user, To_Unbounded_String(username));
+
+      bool := setPassword(user, To_Unbounded_String( password));
+      setContacts(user, contacts);
+
+      User_Maps.Insert(Container => this.users,
+                       Key       => To_Unbounded_String(username),
+                       New_Item  => user);
+      -- # TODO Constraint_Error, wenn Key schon vorhanden, muss abgefangen werden #
+      return true;
+
+   Exception
+      when Constraint_Error =>
+           return false;
+
+   end registerUser;
+
+   function getUser(database : in User_Database; username : in Unbounded_String) return UserPtr is
+   begin
+      return database.users.Element(Key => username);
    end getUser;
 
-   function addUser(this : in out User_Database; userToAdd : in User) return Boolean is
-   begin
-      return false;
-   end addUser;
 
-   function removeUser(this : in out User_Database; userToRem : in User) return Boolean is
+   procedure saveUserDatabase(this : in User_Database) is
+      Write_To : String := To_String(this.databaseFileName);
+      DataFile : File_type;
+      usermap : User_Maps.map :=this.users;
    begin
-      return false;
-   end removeUser;
+      begin
+      Open(File => DataFile,
+           Mode => Out_File,
+           Name => Write_To);
+      exception
+         when others => --# welcher Fehler wird hier normalerweise geworfen? #
+            Create(File => DataFile,
+                   Mode => Out_File,
+                   Name => Write_To);
+      end;
+
+      for user of usermap loop
+         --# schreibe jeden User in eine Line im folgenden Format
+         --# username:userpasswort[:contact]*
+         Put_Line(DataFile, To_String(userToUnboundedString(this => user)));
+      end loop;
+      Close(DataFile);
+   end saveUserDatabase; -- writes User Database to file
+
+   procedure loadUserDatabase(this : in out User_Database) is
+      Read_From : String := To_String(this.databaseFileName);
+      DataFile : File_type;
+      usermap : User_Maps.map;
+   begin
+       begin
+      Open(File => DataFile,
+           Mode => In_File,
+           Name => Read_from);
+      exception
+         when others => --# welcher Fehler wird hier normalerweise geworfen? #
+            Put_line("Userdatabase file doesn't exist.");
+            return;
+      end;
+
+      begin
+         loop
+            declare
+               --# lese Line aus dem File
+               userLine : String := Get_Line(DataFile);
+               --# konvertiere Line in User
+               user : UserPtr := StringToUser(userLine, this);
+            begin
+               null;
+            end;
+         end loop;
+      Exception
+         when others =>
+            Close(DataFile);
+      end;
+   end loadUserDatabase; -- loads user database from file
+
+   function userToUnboundedString(this : in out UserPtr) return Unbounded_String is
+      --result : Unbounded_String := this.user_data.username;
+      result : Unbounded_String := getUsername(this);
+      contacts : ContactList.List := getContacts(this);
+   begin
+      --#Trennzeichen
+      Append(Source   => result,
+             New_Item => To_Unbounded_String(Protocol.Seperator));
+      --#Passwort
+      Append(Source   => result,
+             New_Item => getPassword(this));
+
+      for contact of contacts loop
+         --#Trennzeichen
+         Append(Source   => result,
+                New_Item => To_Unbounded_String(Protocol.Seperator));
+         --#Kontaktname
+         Append(Source   => result,
+              New_Item => getUsername(contact));
+      end loop;
+      return result;
+   end userToUnboundedString;
+
+   function StringToUser(inputLine : in String; database : in User_Database) return UserPtr is
+      MessageParts : GNAT.String_Split.Slice_Set;
+      MessagePart : Unbounded_String;
+      Count : Slice_Number;
+      user : UserPtr;
+      bool : Boolean;
+   begin
+      -- # Nachricht wird an definiertem Trennzeichen zerstueckelt #
+      GNAT.String_Split.Create(S => MessageParts, From => inputLine,
+                               Separators => Protocol.Seperator, Mode => GNAT.String_Split.Multiple);
+      Count := GNAT.String_Split.Slice_Count(MessageParts);
+
+      --#TODO: Pruefen, ob Nachricht richtiges Format
+
+
+      --#Slice 1: username
+      setUsername(user,To_Unbounded_String(Gnat.String_Split.Slice(MessageParts, 1)));
+      --#Slice 2: password
+      bool := setPassword(user,To_Unbounded_String(Gnat.String_split.slice(MessageParts, 2)));
+      --#Slice rest: contacts
+      for i in 3 .. Count loop
+         --slice username, suche username, füge username zur contactliste hinzu
+         declare
+            contactName : Unbounded_String := To_Unbounded_String(Gnat.String_split.slice(MessageParts, i));
+            newContact : UserPtr := GetUser(database      => database ,
+                                         username => contactName);
+         begin
+            bool := addContact(this         => user,
+                       contactToAdd => newContact);
+         end;
+      end loop;
+
+      return user;
+
+   end StringToUser;
 
 end User_Databases;
--- server/user_databases.ads
+++ server/user_databases.ads
@@ -2,27 +2,41 @@ with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 with Ada.Strings.Unbounded.Hash_Case_Insensitive;
 with Ada.Strings.Unbounded.Equal_Case_Insensitive;
 with Ada.Containers.Hashed_Maps;  use Ada.Containers;
+with Ada.Text_IO; use Ada.Text_IO;
+with Protocol;
+with GNAT.String_Split; use GNAT.String_Split;
 
-with Data_Types; use Data_Types;
+with DataTypes; use DataTypes;
 
 package User_Databases is
 -- public
-   type User_Database is private;
-   function getUser(this : in out User_Database; userToGet : in User) return User;
-   function addUser(this : in out User_Database; userToAdd : in User) return Boolean;
-   function removeUser(this : in out User_Database; userToRem : in User) return Boolean;
 
-private
+   type User_Database is tagged private;
+   function registerUser(this : in out User_Database; username : in String; password : in String) return Boolean;
+   function getUser(database : in User_Database; username : in Unbounded_String) return UserPtr;
+
+   procedure saveUserDatabase(this : in User_Database); -- writes User Database to file
+   procedure loadUserDatabase(this : in out User_Database); -- loads user database from file
+
    package User_Maps is new Hashed_Maps
      (Key_Type => Unbounded_String,
-      Element_Type => User,
+      Element_Type => UserPtr,
       Hash => Ada.Strings.Unbounded.Hash_Case_Insensitive,
       Equivalent_Keys => "=");
    use User_Maps;
 
-   type User_Database is record
-      users : User_Maps.Map;
+
+private
+
+   function userToUnboundedString(this : in out UserPtr) return Unbounded_String;
+   function StringToUser(inputLine : in String; database : in User_Database) return UserPtr;
+
+   type User_Database is tagged record
+      users : User_Maps.Map; -- # username -> userDataSet
+      databaseFileName: Unbounded_String := To_Unbounded_String("ADAChatDatabase.txt");
       maxSize : Integer := 1000000;
+
    end record;
+
 
 end User_Databases;
