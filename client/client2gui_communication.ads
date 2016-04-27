0b444365d35930b77d5e44ce48f5c7fbf667fe5b
Ewald Bayer <ewald.bayer@mni.thm.de>
2016-04-27 17:07:07 +0200

    Schnittstellen für die Kommunikation angepasst

--- client/client2gui_communication.ads
+++ client/client2gui_communication.ads
@@ -1,11 +1,16 @@
+with Protocol; use Protocol;
+with dataTypes; use dataTypes;
+
 package Client2Gui_Communication is
 
    type GUI is interface;
 
-   procedure UpdateOnlineUsers(This : in GUI) is abstract;
+   -- Fügt den User der Online Liste hinzu und entfernt ihn aus der Offline Liste (falls vorhanden)
+   procedure AddOnlineUser(This : in GUI; UserName : Unbounded_String) is abstract;
 
-   procedure UpdateOfflineUsers(This : in GUI) is abstract;
+   -- Fügt den User der Offline Liste hinzu und entfernt ihn aus der Online Liste (falls vorhanden)
+   procedure UpdateOfflineUsers(This : in GUI; UserName : Unbounded_String) is abstract;
 
-   procedure ShowChatMessages(This : in GUI) is abstract;
+   procedure ShowChatMessages(This : in GUI; message : Message_Object) is abstract;
 
-   end Client2Gui_Communication;
+end Client2Gui_Communication;
