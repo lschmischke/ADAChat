89d21c2a4721fe25a9cf31eae53c0d4406cd2fc4
Sebastian Westbrock <sebastian.westbrock@mni.thm.de>
2016-04-28 14:12:33 +0200

    Schnittstellenakutalisierungen von Ewalds Branch geholt

--- client/gui2client_communication.ads
+++ client/gui2client_communication.ads
@@ -1,14 +1,22 @@
 with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
+with Protocol; use Protocol;
+with GNAT.Sockets; use GNAT.Sockets;
 
 package Gui2Client_Communication is
 
    type Client is interface;
 
-   procedure LoginUser(This : in out Client; Username : in Unbounded_String; Password : in Unbounded_String) is abstract;
+   -- In der GUI gibt es Einstellungen für Server Adresse und Port, eine Antwort vom Server muss immer verarbeitet werden
+   procedure LoginUser (This : in out Client; Username : in Unbounded_String; Password : in Unbounded_String;
+                        ServerAdress : in Unbounded_String; ServerPort : in Port_Type;
+                        AnswerFromServer : out MessageObject) is abstract;
 
    procedure DisconnectUser(This : in out Client; Username : in Unbounded_String; Message : in Unbounded_String) is abstract;
 
-   procedure RegisterUser(This : in out Client; Username : in Unbounded_String; Password : in Unbounded_String) is abstract;
+   -- Für die Registrierung sollte der entsprechende Server angegeben werden, auf dem sich registriert werden soll
+   procedure RegisterUser(This : in out Client; Username : in Unbounded_String; Password : in Unbounded_String;
+                          ServerAdress : in Unbounded_String; ServerPort : in Port_Type;
+                          AnswerFromServer : out MessageObject) is abstract;
 
    procedure InviteToGroupChat(This : in out Client; Username : in Unbounded_String; Receiver : in Integer;
                                Participant : in Unbounded_String) is abstract;
