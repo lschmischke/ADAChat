80f8c4b6fd36a46e7e0e18ba38fb30c4b4ce4211
Thomas Iffland <Thomas@pool-210-Wlan-Clients-7461.dyn.fh-giessen.de>
2016-05-06 11:23:10 +0200

    Änderungen an der Gui. An Server logic wurde nichts verändert, das rafft der aber nicht :-(

--- server/ServerGuiCallbacks.ads
+++ server/ServerGuiCallbacks.ads
@@ -6,7 +6,7 @@ package ServerGuiCallbacks is
    procedure Quit (Object : access Gtkada_Builder_Record'Class);
    procedure InitializeGuiReferences(myBuilder: access Gtkada_Builder_Record'Class);
 
-procedure rowHandler ( Object : access Gtkada_Builder_Record'Class);
+   procedure rowHandler ( Object : access Gtkada_Builder_Record'Class);
    procedure clicked_button_about ( Object : access Gtkada_Builder_Record'Class);
    procedure clicked_button_server_start ( Object : access Gtkada_Builder_Record'Class);
    procedure clicked_button_server_stop ( Object : access Gtkada_Builder_Record'Class);
